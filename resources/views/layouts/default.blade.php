55c344947542e14f04565dd930abb21b2dc521a7
Penn <penn_z@aliyun.com>
2018-01-28 23:15:22 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,14 @@
     <head>
         <meta charset="utf-8">
         <title>@yield('title', 'Sample')- Laravel 入门教程</title>
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-        @yield('content')
+        @include('layouts._header')
+
+        <div class="container">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
     </body>
 </html>
